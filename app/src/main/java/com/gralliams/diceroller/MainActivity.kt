4c179f6eb78f45c2b727daddf7e4e352eaaaf74b
GreatGrant <greatgrant116@gmail.com>
2023-01-26 15:05:19 +0100

    add logic

--- app/src/main/java/com/gralliams/diceroller/MainActivity.kt
+++ app/src/main/java/com/gralliams/diceroller/MainActivity.kt
@@ -2,10 +2,35 @@ package com.gralliams.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var resultImageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resultImageView = findViewById(R.id.result_imageview)
+        val rollButton: Button = this.findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        var randomInt = Random.nextInt(6) + 1
+        var imageResource = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        resultImageView.setImageResource(imageResource)
     }
 }
